9c3a96f9ac240d5b7d323e64052491736ac98e92
Hercules <abhijio082@gmail.com>
2017-04-16 07:16:51 +0000

    Xenonify tree

---
+++ xenonhd.mk
@@ -0,0 +1,21 @@
+$(call inherit-product, device/asus/Z00L/full_Z00L.mk)
+
+# Inherit some common xenonHD stuff.
+$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
+
+BOARD_VENDOR := asus
+PRODUCT_NAME := xenonhd_Z00L
+PRODUCT_BRAND := asus
+PRODUCT_MANUFACTURER := asus
+PRODUCT_DEVICE := Z00L
+PRODUCT_MODEL := Zenfone 2 Laser
+TARGET_VENDOR := asus
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+ROOT_METHOD=magisk
+
+PRODUCT_PACKAGES += \
+    KernelAdiutor \
+    MiXplorer
+
+PRODUCT_PROPERTY_OVERRIDES += ro.xenonhd.maintainer="nick name"The_Hercules"
